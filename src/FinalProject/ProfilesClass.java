f48edccc9175828b3d20fe54dfd6066e3cf5b0ac
Vaughn01 <tv111790@students.davenport.k12.ia.us>
2017-03-08 13:55:21 -0600

    Update again 3/8/2017

--- src/FinalProject/ProfilesClass.java
+++ src/FinalProject/ProfilesClass.java
@@ -2,7 +2,7 @@
  * 
  */
 package FinalProject;
-
+import javax.swing.JOptionPane;
 /**
  * @author tv111790
  *
@@ -12,8 +12,31 @@ public class ProfilesClass {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
+		JOptionPane.showMessageDialog(null, "Oh him… That’s just John. He is a huge pain in the… "
+				+ "wait I forgot you are a new classmate. He is the teacher, BUT…. he  is so annoying. "
+				+ "He keeps messing with my projects and distracting me! So good luck trying to get anything done around him… "
+				+ "Good teacher though and he knows his stuff..");
+		JOptionPane.showMessageDialog(null, "That’s Ress…. I cannot pronounce her last name… Anyway actually now that you mention her, "
+				+ "I don’t know much about her. She seems nice, but then again not everyone who seems nice is…  ");
+		JOptionPane.showMessageDialog(null, "That’s Tatyana. She knows everything, so if you have any questions, "
+				+ "she’s the person to ask… although… she is a little bit mysterious so no one really approaches her. "
+				+ "She seems scary so do not ask me to approach her…");
+		JOptionPane.showMessageDialog(null, "Well duh, you already know my name! Were you not listening to me before? Well I’m Charles! "
+				+ "I have had 3 classes with Mr. Brosius so if you need anything, you can ask me… I know just about everything, except github. "
+				+ "DO NOT ASK ME ABOUT GITHUB! ");
+		JOptionPane.showMessageDialog(null, "That’s Kassie, she knows ");
+		JOptionPane.showMessageDialog(null, "Who are you talking…. WOW! He is actually here today!!! Usually he NEVER shows up! "
+				+ "There must be a special reason that he is here today… hmm… I don’t know. Anyway that’s Nick, why he is still trying to take"
+				+ " the class I don’t know.");
+		JOptionPane.showMessageDialog(null, "That is Mason");
+		JOptionPane.showMessageDialog(null, "Oh that is Sebastian. He is the teacher’s pet. You can count on him to be on his side. He agrees with everything he says..");
+		JOptionPane.showMessageDialog(null, "That is Zach. He is ");
+		JOptionPane.showMessageDialog(null, "That is Chase.");
+		JOptionPane.showMessageDialog(null, "That is Isaiah. He complains about everything!!! But then he never does any work. ");
+		JOptionPane.showMessageDialog(null, "That is Mason. ");
 
 	}
 
